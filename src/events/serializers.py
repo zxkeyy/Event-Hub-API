944ab2b0034b0fa5d72ede710a6f430f08188821
zxkeyy <z_selmani@estin.dz>
2023-08-31 17:14:55 +0100

    made owner visible in get  responses for events and clubs

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -4,7 +4,7 @@ from .models import Category, Club, Event, University, Tag
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'name', 'location_name', 'location_id', 'wilaya', 'number_attendants', 'description', 'image', 'start_date', 'end_date', 'body', 'tags', 'category', 'clubs', 'universities', 'slug', 'is_verified']
+        fields = ['id', 'name', 'location_name', 'location_id', 'wilaya', 'number_attendants', 'description', 'image', 'start_date', 'end_date', 'body', 'tags', 'category', 'clubs', 'universities', 'slug', 'owner', 'is_verified']
 
         lookup_field = 'slug'
         read_only_fields = ['is_verified']
@@ -12,7 +12,7 @@ class EventSerializer(serializers.ModelSerializer):
 class ClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Club
-        fields = ['id', 'name', 'description', 'image', 'body', 'universities', 'events']
+        fields = ['id', 'name', 'description', 'image', 'body', 'universities', 'owner', 'events']
 
         read_only_fields = ['events']
 
